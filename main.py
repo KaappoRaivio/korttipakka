6cb5378270f87f2c24bc9633a097d9e81825ec05
NoSocksForYou <kaappo.raivio@gmail.com>
2018-02-14 11:33:15 +0200

    init

---
+++ main.py
@@ -0,0 +1,67 @@
+import random
+
+
+class Kortti(object):
+    def __init__(self, maa=None, arvo=None, paljastettu=False):
+        self.maa = maa
+        self.arvo = arvo
+        self.paljastettu = paljastettu
+
+    def __str__(self):
+        if not self.paljastettu:
+            return "hidden"
+        return "{}-{}".format(self.maa, self.arvo)
+
+
+class Korttipakka(object):
+    def __init__(self, maat=['Ruutu', 'Risti', 'Hertta', 'Pata'], arvot=range(2, 15)):
+        self.kortit = []
+        self.maat = maat
+        self.arvot = arvot
+        for i in maat:
+            for a in arvot:
+                self.kortit.append(Kortti(i, a))
+
+    def __str__(self):
+        temp = ''
+        for i in self.kortit:
+            temp += str(i)
+            temp += ', '
+        return temp
+
+    def shuffle(self):
+        for i in range(len(self.kortit) - 1, 0, -1):
+            random_indeksi = random.randint(0, i - 1)
+            self.kortit[random_indeksi], self.kortit[i] = self.kortit[i], self.kortit[random_indeksi]
+
+    def otaKorttiPakanPäältä(self):
+        kortti = self.kortit[0]
+        self.kortit = self.kortit[1:]
+        return kortti
+
+    def laitaKorttiPakkaan(self, kortti):
+        if str(kortti) in str(self.kortit):
+            raise Exception('Kortti on jo pakassa!')
+        else:
+            # self.kortit = self.kortit[:random_paikka], kortti, self.kortit[random_paikka + 1:]
+            self.kortit.append(kortti)
+            self.shuffle()
+
+
+class Template(object):
+    def __init__(self, lista):
+        self.lista = lista
+
+
+class Pelaaja(object):
+    def __init__(self, nimi):
+        self.nimi = nimi
+        self.käsi = []
+
+    def jaaKortit(self, template):
+        pass
+
+pokerikäsi = Template([Kortti(paljastettu=False) * 5])
+
+korttipakka = Korttipakka()
+korttipakka.shuffle()
