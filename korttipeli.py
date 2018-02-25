8e6b07c01e2c09830408d280c22214f46019f190
NoSocksForYou <kaappo.raivio@gmail.com>
2018-02-25 14:51:55 +0100

    dealing "sorta" works

--- korttipeli.py
+++ korttipeli.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 
 import random
+from stack import Stack
 
 
 class Card(object):
@@ -13,11 +14,14 @@ class Card(object):
         visual_representation_closed = {'diamonds': '♢', 'clubs': '♧', 'spades': '♤', 'hearts': '♡'}
         visual_representation_open = {'diamonds': '♦', 'clubs': '♣', 'spades': '♠', 'hearts': '♥'}
 
+        if None in [self.suit, self.value]:
+            return 'Undef'
         special_values = {**{11: 'J', 12: 'Q', 13: 'K', 14: 'A'}, **{x: x for x in range(11)}}
         if self.visible:
             return '{} {}'.format(visual_representation_open[self.suit], special_values[self.value])
         else:
             return '{} {}'.format(visual_representation_closed[self.suit], special_values[self.value])
+        return ''
 
 
 class DeckOfCards(object):
@@ -42,64 +46,107 @@ class DeckOfCards(object):
             self.cards[random_index], self.cards[i] = self.cards[i], self.cards[random_index]
 
     def drawCard(self):
-        return self.cards.pop()
+        temp = self.cards.pop()
+        return temp
 
 
 class Player(object):
-    def __init__(self, name):
+    def __init__(self, name, RFG_pile=False):
         self.hand = []
         self.name = name
+        self.RFG_pile = RFG_pile
+        if RFG_pile:
+            self.RFG_pile = []
 
-    def __setattr__(self, name, value):
-        if name == 'hand':
-            super(Player, self).__setattr__(name, value)
-            super(Player, self).__setattr__('amount_of_cards', len(self.hand))
-        else:
-            super(Player, self).__setattr__(name, value)
-
+    @property
+    def amount_of_cards(self):
+        return len(self.hand)
 
     def __str__(self):
         temp = []
         for i in self.hand:
-            temp.append(str(i))
+            print(i.peek())
+            temp.append(str(i.peek()))
         return ', '.join(temp)
 
-    def addCardToHand(self, suit, value, visible):
-        self.hand = self.hand + [Card(suit, value, visible)]
+    def addCardToHand(self, stack):
+        self.hand.append(stack)
 
 
 class Table(object):
     def __init__(self, size_x, size_y, players, deck):
         self.size_x = size_x
         self.size_y = size_y
+        self.cards = [[Card() for i in range(size_x)] for i in range(size_y)]
         self.players = players
         self.deck = deck
 
-    def deal(self, template):
-        for player in self.players:
-            for template in template.cards_to_players:
-                card = self.deck.drawCard()
-                player.addCardToHand(card.suit, card.value, template.visible)
+
+    def deal(self, player_template, table_template):
+        for stack in player_template:
+            temp_iterator = stack.stack
+            temp_stack = Stack()
+
+            while temp_iterator.isEmpty() is not True:
+                template_card = temp_iterator.pop()
+                real_card = self.deck.drawCard()
+
+                real_card.visible = template_card.visible
+
+                temp_stack.push(real_card)
+
+            for player in self.players:
+                player.addCardToHand(temp_stack)
 
 
-class DealTemplate(object):
-    def __init__(self, cards_to_table, cards_to_players):
-        self.cards_to_table = cards_to_table
-        self.cards_to_players = cards_to_players
+
+        for y in table_template:
+            for x in y:
+                self.cards[x][y] = self.deck.drawCard()
+                self.cards[x][y].visible = x.visible
 
 
+# class DealTemplate(object):
+#     def __init__(self, cards_to_table, player_hand):
+#
 
-bismarkin_käsi = [Card(visible=True) for i in range(10)] + [Card(visible=False) for i in range(6)]
+class PLayerHandTemplate(object):
+    def __init__(self, closed_dim_x, closed_dim_y=1, open_dim_x=0, open_dim_y=0):
+        self.closed_cards = [[Stack(Card(visible=False)) for x in range(closed_dim_x)] for y in range(closed_dim_y)]
+        self.open_cards = [[Stack(Card(visible=True)) for x in range(open_dim_x)] for y in range(open_dim_y)]
 
+
+class Cell(object):
+    def __init__(self, id, *args):
+        self.stack = Stack(*args)
+
+    def __str__(self):
+        return 'type:<Cell>; ' + str(self.stack.peek())
+
+    @property
+    def how_many_stacked(self):
+        return len(self.stack)
+
+
+bismark_cell = Cell(Card(visible=False), Card(visible=True))
+bismark_cell_hand = Cell(Card(visible=False))
+
+bismarkin_käsi = []
+
+for i in range(10):
+    bismarkin_käsi.append(Cell(i, Card(visible=False), Card(visible=True)))
+
+# bismarkin_käsi = [Cell(Card(visible=False), Card(visible=True)), Cell(Card(visible=False), Card(visible=True)), Cell(Card(visible=False), Card(visible=True)), Cell(Card(visible=False), Card(visible=True)), Cell(Card(visible=False), Card(visible=True)), Cell(Card(visible=False), Card(visible=True)), Cell(Card(visible=False), Card(visible=True)), Cell(Card(visible=False), Card(visible=True)), Cell(Card(visible=False), Card(visible=True)), Cell(Card(visible=False), Card(visible=True)), ]
 
 deck = DeckOfCards(52)
 deck.shuffle()
 
-template = DealTemplate([], bismarkin_käsi)
+players = [Player('kaappo'), Player('kaappo2')]
 
-kaappo = Player('kaappo')
+bismarkin_pöytä = Table(0, 0, players, deck)
+bismarkin_pöytä.deal(bismarkin_käsi, [])
 
-table = Table(1, 1, [kaappo], deck)
-
-table.deal(template)
-print(kaappo)
+for i in players:
+    print(i.name)
+    for y in i.hand:
+        print(y)
---
+++ stack.py
@@ -0,0 +1,69 @@
+class Stack:
+    def __init__(self, *args):
+        self.__items = []
+        self.push(*args)
+
+    def __str__(self):
+        return str(self.peek())
+
+    def __bool__(self):
+        return not self.isEmpty()
+
+    def __list__(self):
+        return self.__items
+
+
+    def __eq__(self, other):
+        if isinstance(self, type(other)):
+            return self.peek() == other.peek()
+        else:
+            return self.peek() == other
+
+    def __ne__(self, other):
+        if isinstance(self, type(other)):
+            return self.peek() != other.peek()
+        else:
+            return self.peek() != other
+
+    def __gt__(self, other):
+        if isinstance(self, type(other)):
+            return self.peek() > other.peek()
+        else:
+            return self.peek() > other
+
+    def __lt__(self, other):
+        if isinstance(self, type(other)):
+            return self.peek() < other.peek()
+        else:
+            return self.peek() < other
+
+    def __ge__(self, other):
+        if isinstance(self, type(other)):
+            return self.peek() >= other.peek()
+        else:
+            return self.peek() >= other
+
+    def __le__(self, other):
+        if isinstance(self, type(other)):
+            return self.peek() <= other.peek()
+        else:
+            return self.peek() <= other
+
+
+    def isEmpty(self):
+        return self.__items == []
+
+    def push(self, *args):
+        for item in args:
+            self.__items.append(item)
+
+    def pop(self):
+        return self.__items.pop()
+
+    def peek(self):
+        temp = self.pop()
+        self.push(temp)
+        return temp
+
+    def __len__(self):
+        return len(self.__items)
